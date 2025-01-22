d09a439de79e8109e1f64b43fb00777305ff07ba
Michael Bolanos <michael.bolanos@whirlwindsteel.com>
2025-01-22 23:02:29 +0000

    Add integration tests for TokenRangeService
    Implemented integration tests to validate TokenRangeService functionality, including token range assignment and ensuring uniqueness across multiple requests. Added a Fixture with PostgreSQL Testcontainers support and initialized the test infrastructure. Updated the solution to include the new test project.

---
+++ TokenRanges/tests/UrlShortener.TokenRangeService.Tests/Fixture.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Npgsql;
+using Testcontainers.PostgreSql;
+
+namespace UrlShortener.TokenRangeService.Tests;
+
+public class Fixture : WebApplicationFactory<ITokenRangeAssemblyMarker>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgreSqlContainer;
+    public string ConnectionString => _postgreSqlContainer.GetConnectionString();
+
+    public Fixture()
+    {
+        _postgreSqlContainer = new PostgreSqlBuilder().Build();
+    }
+    
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+        Environment.SetEnvironmentVariable("Postgres__ConnectionString", ConnectionString);
+        
+        await InitializeTokenRangesTable();
+    }
+
+    private async Task InitializeTokenRangesTable()
+    {
+        var tokenRangesTable = await File.ReadAllTextAsync("TokenRangesTable.sql");
+        await using var connection = new NpgsqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        
+        await using var command = new NpgsqlCommand(tokenRangesTable, connection);
+        await command.ExecuteNonQueryAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _postgreSqlContainer.StopAsync();
+    }
+}
